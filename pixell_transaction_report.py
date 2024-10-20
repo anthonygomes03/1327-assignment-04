37efc90da95808f4d1304d3338ad0bb3e0f12052
rrc_F2024_agomes62 <agomes62@academic.rrc.ca>
2024-10-20 13:24:59 -0500

    I found that transaction counter was redundant and I only needed transaction count.

--- pixell_transaction_report.py
+++ pixell_transaction_report.py
@@ -14,7 +14,6 @@ valid_transaction_types = ['deposit', 'withdraw']
 customer_data = {}
 rejected_records = []
 transaction_count = 0
-transaction_counter = 0
 total_transaction_amount = 0
 valid_record = True
 error_message = ''
@@ -83,7 +82,7 @@ try:
             amount, type = transaction
             print(f"\t{type.capitalize()}: {amount}")
 
-    print(f"\nAVERAGE TRANSACTION AMOUNT: {(total_transaction_amount / transaction_counter)}")
+    print(f"\nAVERAGE TRANSACTION AMOUNT: {(total_transaction_amount / transaction_count)}")
 
     print("\nREJECTED RECORDS\n================")
     for record in rejected_records:
